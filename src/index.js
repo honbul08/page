961bb638a260182b312b4e85174f310e86c856ce
이예지 <yejilee2008@naver.com>
2025-01-10 20:41:15 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+// yarn add gh-pages
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
